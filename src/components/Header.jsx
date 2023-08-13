2cdb91996fd03d44658d9fd19bb0bae22954e26c
1998KB <klajdibeqiraj88@gmail.com>
2023-08-13 20:40:02 +0200

    depl fix3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,14 @@ export const Header = () => {
     <header className="flex items-center py-2 px-4 justify-center">
       <div className="flex g-2 items-center">
         <img src={Logo} className="w-14 md:w-12" />
-        <div className="text-3xl md:text-xl">Rocket Team</div>
+        <div className="text-3xl md:text-2xl">Rocket Team</div>
       </div>
       <div className="flex-grow justify-end items-center gap-10 hidden md:flex">
         <label>{t("home")}</label>
         <label>{t("services")}</label>
         <label>{t("development")}</label>
         <label>{t("faq")}</label>
-        <button>Contact</button>
+        <button className="py-2">Contact</button>
       </div>
     </header>
   );
