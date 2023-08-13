0a722921a97894f31a77e46f3ef2642704f4e833
Gonzalo <huntymonster@gmail.com>
2023-08-13 14:27:09 +0200

    hero

---
+++ src/assets/content/hero.js
@@ -0,0 +1,20 @@
+import Img1 from "../images/hero/expertIcon.png";
+import Img2 from "../images/hero/rocketIcon.png";
+import Img3 from "../images/hero/puzzleIcon.png";
+
+const hero = [
+  {
+    title: "Digital Experts",
+    image: Img1,
+  },
+  {
+    title: "Light-speed Delivery",
+    image: Img2,
+  },
+  {
+    title: "Personalised Solutions",
+    image: Img3,
+  },
+];
+
+export default hero;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import BGImage from "../assets/images/hero/heroWave.png";
+import HeroCards from "../assets/content/hero";
 
 export const Hero = () => {
   const { t } = useTranslation();
 
   return (
-    <>
+    <div className="flex flex-col justify-center items-center gap-6 mt-20">
+      <img src={BGImage} className="absolute w-screen top-0 -z-10 h-3/5" />
       <h1>{t("heroTitle")}</h1>
       <p>{t("heroSubtitle")}</p>
       <button className="bg-black text-white">{t("heroCTA")}</button>
-    </>
+      <div className="flex justify-center items-center gap-10 mt-20 invisible md:visible w-full">
+        {HeroCards.map((card, i) => (
+          <div
+            key={i}
+            className="flex sm:flex-col justify-center items-center gap-6 w-1/4"
+          >
+            <img src={card.image} className="w-20" />
+            <label>{t(card.title)}</label>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
