a39454afc8f747a9c41385336a775ecd222bbeba
1998KB <klajdibeqiraj88@gmail.com>
2023-08-13 14:26:17 +0200

    services

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import { services } from "../assets/content/services";
+import BGimage from "../assets/images/services/BGimage.png";
 
 export const Services = () => {
-  return <div>Services</div>;
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex flex-col gap-10 ">
+      <img
+        src={BGimage}
+        className="absolute w-screen left-0 h-x sm:h-5/6 md:h-3/5 "
+      />
+      <div>
+        <h2>{t("servicesTitle")}</h2>
+        <p>{t("servicesSubtitle")}</p>
+      </div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-10">
+        {services.map((service, index) => (
+          <div
+            className="flex flex-col justify-center items-center gap-2"
+            key={index}
+          >
+            <label>{t(service.text)}</label>
+            <img className="w-20" src={service.image} alt={service.alt} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
