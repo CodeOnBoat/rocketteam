56c3fc0ba72ab922511b508f63fb998004532484
Gonzalo <huntymonster@gmail.com>
2023-08-13 18:25:25 +0200

    faq

---
+++ src/assets/content/faq.js
@@ -0,0 +1,24 @@
+const faq = [
+  {
+    question: "What is the difference between a full node and a light node?",
+    answer:
+      "A full node is a node that contains the entire blockchain. A light node is a node that only contains the block headers and the transactions that are relevant to the node. A light node is also known as a SPV node (Simplified Payment Verification).",
+  },
+  {
+    question: "What is the difference between a full node and a light node?",
+    answer:
+      "A full node is a node that contains the entire blockchain. A light node is a node that only contains the block headers and the transactions that are relevant to the node. A light node is also known as a SPV node (Simplified Payment Verification).",
+  },
+  {
+    question: "What is the difference between a full node and a light node?",
+    answer:
+      "A full node is a node that contains the entire blockchain. A light node is a node that only contains the block headers and the transactions that are relevant to the node. A light node is also known as a SPV node (Simplified Payment Verification).",
+  },
+  {
+    question: "What is the difference between a full node and a light node?",
+    answer:
+      "A full node is a node that contains the entire blockchain. A light node is a node that only contains the block headers and the transactions that are relevant to the node. A light node is also known as a SPV node (Simplified Payment Verification).",
+  },
+];
+
+export default faq;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
       spacing: {
         x: "70rem",
       },
+      backgroundImage: {
+        gradientFaq:
+          "linear-gradient(180deg, rgba(80, 235, 211, 0.10) 0%, rgba(255, 0, 255, 0.04) 100%)",
+      },
     },
   },
   plugins: [],
