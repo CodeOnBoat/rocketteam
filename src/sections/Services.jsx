8507d7408b038639328dcb24f3eb444489448221
Gonzalo <huntymonster@gmail.com>
2023-08-14 19:17:13 +0200

    fix cards

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -23,7 +23,7 @@ export const Services = ({ servicesRef }) => {
         <h2>{t("servicesTitle")}</h2>
         <p>{t("servicesSubtitle")}</p>
       </motion.div>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 pb-10">
         {services.map((service, index) => (
           <motion.div
             initial={{ opacity: 0, x: 100 }}
