1b9955d9b9ab0c1de287601172dd6d4a014b5cb8
Gonzalo <huntymonster@gmail.com>
2023-08-18 12:33:55 +0200

    fixed iphone header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,10 +24,10 @@ export const Header = ({ servicesRef, rocketWayRef, contactRef }) => {
   }, [i18n.language]);
 
   return (
-    <header className="flex items-center py-2 px-4 justify-center mt-2 sm:mt-0">
+    <header className="flex items-center py-2 px-4 justify-between md:justify-center mt-2 sm:mt-0">
       <div className="flex g-2 items-center">
         <img src={Logo} className=" w-14 md:w-12" />
-        <div className="text-3xl md:text-2xl tracking-widest">Rocket Team</div>
+        <div className="text-2xl tracking-widest">Rocket Team</div>
       </div>
       <div className="flex-grow justify-end items-center gap-10 hidden lg:flex pr-5 mr-12">
         <label
